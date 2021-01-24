09407a19a37c05b7c4557d525f5b9d890e49fede
adrianvilchez <avilchez758@gmail.com>
2021-01-24 16:09:04 +0100

    Componente Header utilizando styled components

---
+++ src/components/Header.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import styled from '@emotion/styled';
+
+const ContenedorHeader = styled.header`
+    background-color: #159463;
+    padding: 10px;
+    font-weight: bold;
+    color: #ffffff;
+`;
+
+const TextoH1 = styled.h1`
+    font-size: 2rem;
+    margin: 0;
+    font-family: 'Roboto 24', sans-serif;
+    text-align: center;
+`;
+
+const Header = ({ titulo }) => {
+    return (
+        <ContenedorHeader>
+            <TextoH1>{ titulo }</TextoH1>
+        </ContenedorHeader>
+    );
+}
+
+export default Header;
