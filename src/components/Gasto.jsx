5c0593ae5c6bd1a7790f6dba9dd9255dcc108d06
adrianvilchez <avilchez758@gmail.com>
2021-01-31 16:20:59 +0100

    Implementamos transición cuando obtengamos nuevos datos de vehículos

--- src/components/Gasto.jsx
+++ src/components/Gasto.jsx
@@ -4,9 +4,10 @@ import { tipoConsumo } from '../helper';
 import { tipoCoste } from '../helper';
 
 import styled from '@emotion/styled';
+import { TransitionGroup, CSSTransition } from 'react-transition-group';
+
 
 const ContenedorGasto = styled.div`
-    text-align: center;
     background-color: #0d5a3d;
     color: #FFFFFF;
     padding: 1em;
@@ -14,7 +15,7 @@ const ContenedorGasto = styled.div`
 `;
 
 const UlLista = styled.ul`
-    text-align: left; 
+    text-align: left;
 `;
 
 const Gasto = ( { datos } ) => {
@@ -28,19 +29,32 @@ const Gasto = ( { datos } ) => {
     return (
         <Fragment>
             <ContenedorGasto>
-                <UlLista>
-                    <li>
-                        Consumo/100km: { consumo } { tipoConsumo(combustible) }
-                    </li>
-                    <li>
-                    { tipoCoste(combustible) }: { costeKm } €
-                    </li>
-                    <li>
-                        Coste Total: { costeTotal } €
-                    </li>
-                </UlLista>
+                <TransitionGroup
+                    component = 'p'
+                    className = 'resultado'
+                >
+                    <CSSTransition
+                        classNames ='resultado'
+                        key = { costeKm }
+                        timeout = {{
+                            enter: 500,
+                            exit: 500
+                        }}
+                    >
+                        <UlLista>
+                            <li>
+                                <p>Consumo/100km: { consumo } { tipoConsumo(combustible) }</p>
+                            </li>
+                            <li>
+                            <p>{ tipoCoste(combustible) }: { costeKm } €</p>
+                            </li>
+                            <li>
+                                <p>Coste Total: { costeTotal } €</p>
+                            </li>
+                        </UlLista>
+                    </CSSTransition>
+                </TransitionGroup>
             </ContenedorGasto>
-
         </Fragment>
     )
 }
